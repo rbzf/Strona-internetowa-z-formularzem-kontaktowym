fbe98902597c6d58868b14fd070672204af54e14
rbzf <anna.sadkowska@gmail.com>
2023-07-05 12:26:38 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/as/formularz_konktaktowy/StronaInternetowaZFormularzemKontaktowymApplication.java
@@ -0,0 +1,13 @@
+package pl.as.formularz_konktaktowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaInternetowaZFormularzemKontaktowymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaInternetowaZFormularzemKontaktowymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/as/formularz_konktaktowy/StronaInternetowaZFormularzemKontaktowymApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.as.formularz_konktaktowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StronaInternetowaZFormularzemKontaktowymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
