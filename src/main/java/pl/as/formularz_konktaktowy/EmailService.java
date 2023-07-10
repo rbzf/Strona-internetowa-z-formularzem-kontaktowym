6a62e8f214fd293bdf3fd4aada924d9d19560556
rbzf <anna.sadkowska@gmail.com>
2023-07-10 08:15:30 +0200

    Gdzie robie blad w wysylaniu wiadomosci?

--- src/main/java/pl/as/formularz_konktaktowy/EmailService.java
+++ src/main/java/pl/as/formularz_konktaktowy/EmailService.java
@@ -1,7 +1,26 @@
 package pl.as.formularz_konktaktowy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String receiver, String sender, String subject, String text){
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(receiver);
+        mail.setFrom(sender);
+        mail.setSubject(subject);
+        mail.setText(text);
+        javaMailSender.send(mail);
+    }
 }
--- src/main/java/pl/as/formularz_konktaktowy/MainController.java
+++ src/main/java/pl/as/formularz_konktaktowy/MainController.java
@@ -2,9 +2,22 @@ package pl.as.formularz_konktaktowy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    private final EmailService emailService;
+
+
+    public MainController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+    @RequestMapping("/")
+    @ResponseBody
+    public void send() {
+        emailService.sendMail("rbzf@byom.de", "rbzf@mail.com", "wiadomosc tesowa", "tekst wiadomosci");
+    }
 
 
     @GetMapping("/")
